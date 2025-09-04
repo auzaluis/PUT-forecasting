df09788d85e5e092042db7abe1eb0f7c7a885fb6
auzaluis <luis.auza@gmail.com>
2025-09-03 22:49:34 -0400

    wip

--- app.R
+++ app.R
@@ -15,53 +15,81 @@ df <- read_parquet("data/raw_data.parquet") |>
   )
 
 ui <- fluidPage(
+  
   titlePanel("People Using TV Forecasting"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("age_range", "Age Range:", choices = unique(df$age_range)),
-      selectInput("daypart", "Daypart:", choices = unique(df$daypart)),
+      dateInput(
+        "date", "From:",
+        value = min(df$date)
+      ),
+      selectInput(
+        "age_range",
+        "Age Range:",
+        choices = unique(df$age_range)
+      ),
+      selectInput(
+        "daypart",
+        "Daypart:",
+        choices = unique(df$daypart)
+      ),
       pickerInput(
         "hour", "Hour:",
-        choices = sort(unique(df$hour)),
-        selected = 20,
+        choices = NULL,
+        selected = NULL,
         multiple = T,
         options = pickerOptions(
           actionsBox = T,
           liveSearch = T,
           size = 8
         )
-      ),
-      dateRangeInput("date_range", "Date Range:",
-                     start = min(df$date),
-                     end = max(df$date))
+      )
     ),
     mainPanel(
-      plotOutput("putPlot"),
+      plotlyOutput("putPlot"),
       tableOutput("summaryTable")
     )
   )
 )
 
 server <- function(input, output, session) {
-  filtered <- reactive({
+  
+  observe({
+    x <- df |> 
+      filter(
+        daypart == input$daypart,
+        age_range == input$age_range,
+        date >= input$date
+      )
+    
+    updatePickerInput(
+      session,
+      "hour",
+      choices = sort(unique(x$hour)),
+      selected = sort(unique(x$hour))
+    )
+  })
+  
+  df1 <- reactive({
     df %>%
       filter(
         daypart == input$daypart,
         age_range == input$age_range,
-        date >= input$date_range[1],
-        date <= input$date_range[2]
+        hour %in% input$hour
       )
   })
   
-  output$putPlot <- renderPlot({
-    ggplot(filtered(), aes(x = date, y = PUTs)) +
-      geom_line() +
-      labs(title = "PUTs Over Time",
-           x = "Date", y = "PUTs")
+  output$putPlot <- renderPlotly({
+    ggplotly(
+      ggplot(df1(), aes(x = date, y = PUTs)) +
+        geom_line() +
+        labs(title = "PUTs Over Time",
+             x = "Date", y = "PUTs")
+    )
   })
   
   output$summaryTable <- renderTable({
-    filtered() |> 
+    df1() |> 
       summarise(
         mean_PUTs = mean(PUTs, na.rm = TRUE),
         sd_PUTs = sd(PUTs, na.rm = TRUE),
@@ -69,6 +97,7 @@ server <- function(input, output, session) {
         max_PUTs = max(PUTs, na.rm = TRUE)
       )
   })
+  
 }
 
 shinyApp(ui, server)
